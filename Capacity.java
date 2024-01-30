5a70334f9af38a162c133c858a5515dfb024ead0
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-01-30 21:17:00 +0530

    import java.util.Scanner; public class Capacity{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         int s = sc.nextInt();         int b = sc.nextInt();         int a = (2*t*s*b*512)/1024;         System.out.printf("%d KB",a);     } }

---
+++ Capacity.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Capacity{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int s = sc.nextInt();
+        int b = sc.nextInt();
+        int a = (2*t*s*b*512)/1024;
+        System.out.printf("%d KB",a);
+    }
+}
