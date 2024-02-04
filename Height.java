d77e80030d63e43a478a6700210a3b352b5191fe
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-02-04 17:30:13 +0530

    import java.util.Scanner; public class Height{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         if(x<y)         {             System.out.print(y);         }         else         {             System.out.print(x);         }     } }

---
+++ Height.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Height{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if(x<y)
+        {
+            System.out.print(y);
+        }
+        else
+        {
+            System.out.print(x);
+        }
+    }
+}
