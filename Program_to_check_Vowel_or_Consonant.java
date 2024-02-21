b30a835b2e90555ac0a0b44925b19ead607289cf
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-02-21 19:30:50 +0530

    import java.util.*; public class Alpha{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         char a = sc.next().charAt(0);         if(a=='A')         {             System.out.print("Vowel");         }         else if(a=='E')         {             System.out.print("Vowel");         }         else if(a=='I')         {             System.out.print("Vowel");         }         else if(a=='O')         {             System.out.print("Vowel");         }         else if(a=='U')         {             System.out.print("Vowel");         }         else if(a=='a')         {             System.out.print("Vowel");         }         else if(a=='e')         {             System.out.print("Vowel");         }         else if(a=='i')         {             System.out.print("Vowel");         }         else if(a=='o')         {             System.out.print("Vowel");         }         else if(a=='u')         {             System.out.print("Vowel");         }         else         {             System.out.print("Consonant");         }     } }

---
+++ Program_to_check_Vowel_or_Consonant.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Alpha{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        char a = sc.next().charAt(0);
+        if(a=='A')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='E')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='I')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='O')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='U')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='a')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='e')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='i')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='o')
+        {
+            System.out.print("Vowel");
+        }
+        else if(a=='u')
+        {
+            System.out.print("Vowel");
+        }
+        else
+        {
+            System.out.print("Consonant");
+        }
+    }
+}
