a8ee09e4fe223038f299f11de54a620ddb7c8841
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-03-07 10:06:41 +0530

    import java.util.*; public class Table{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int a = sc.nextInt();         int b = sc.nextInt();         for(int i=a;i<=b;i++)         {             System.out.printf("%d x %d = %d ",n,i,n*i);         }     } }

---
+++ Multiplication_Table_-_3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Table{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=a;i<=b;i++)
+        {
+            System.out.printf("%d x %d = %d
+",n,i,n*i);
+        }
+    }
+}
