593b00574f03f14c092ad48adb0dd761d6763248
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-03-14 11:41:51 +0530

    import java.util.*; public class Pretty{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         char b = a.charAt(6);         System.out.print(b);     } }

---
+++ Lucky_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Pretty{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        char b = a.charAt(6);
+        System.out.print(b);
+    }
+}
