de4be5304373e401c98e02cbd497189895a4b393
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-03-14 21:13:20 +0530

    import java.util.*; public class Copy{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         String b = a;         System.out.print(b);     } }

---
+++ String_Copy.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Copy{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = a;
+        System.out.print(b);
+    }
+}
