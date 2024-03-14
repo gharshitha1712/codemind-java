864cd1c4c607ade6170fb1002a243b37c60bbb8c
gharshitha1712 <121536740+gharshitha1712@users.noreply.github.com>
2024-03-14 21:15:31 +0530

    import java.util.*; public class Length{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         int b = a.length();         System.out.print(b);     } }

---
+++ String_Length.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Length{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        int b = a.length();
+        System.out.print(b);
+    }
+}
